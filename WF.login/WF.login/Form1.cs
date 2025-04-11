cd469843212d1d2707f7f4ac9b1fd001e2cf8c44
CHARLESKUNDEVITALE <CHARLESKUNDEVITALE7@gmail.com>
2025-04-10 21:46:37 -0300

    basci early 200 or 90s stlye login screen to black messa reserch facility

--- WF.login/WF.login/Form1.cs
+++ WF.login/WF.login/Form1.cs
@@ -6,5 +6,35 @@ namespace WF.login
         {
             InitializeComponent();
         }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void contextMenuStrip2_Opening(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
     }
 }
